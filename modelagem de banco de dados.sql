aa6e02313ba1eff19cc3f2b95d04d9ada3d1f09b
BrunoLeon007 <brunob734@gmail.com>
2024-12-10 21:20:36 -0300

    Add files via upload

---
+++ modelagem de banco de dados.sql
@@ -0,0 +1,19 @@
+create database ecommerce;
+create database pizzaria;
+drop database ecommerce;
+drop database ex01;
+drop database ecommerce;
+drop database pizzaria;
+drop database pizzaria;
+create database flamingo1;
+create database flamingo2;
+use flamingo1;
+create table usuario(
+idUsuario int not null auto_increment primary key,
+nomeUsuario varchar(255) not null,
+senhaUsuario varchar(80)not null);
+show databases;
+drop database flamingo1;
+drop database flamingo2;
+
+
